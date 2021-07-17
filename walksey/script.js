c25323456f900462c2d3bba05042f7f03ede1995
MrozKarol <polityk357@gmail.com>
2021-07-17 17:13:42 +0200

    WIP slider2

--- walksey/script.js
+++ walksey/script.js
@@ -11,3 +11,17 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
+
+var swiper = new Swiper(".mySwiper1", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+});
