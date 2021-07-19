91f398cc3240738e49885b31078e42c381f1b213
MrozKarol <polityk357@gmail.com>
2021-07-19 20:04:14 +0200

    added slider2

--- walksey/script.js
+++ walksey/script.js
@@ -16,10 +16,11 @@ var swiper = new Swiper(".mySwiper1", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
+    initialSlide: 1,
     slidesPerView: "auto",
     coverflowEffect: {
         rotate: 50,
-        stretch: 0,
+        stretch: 1,
         depth: 100,
         modifier: 1,
         slideShadows: true,
